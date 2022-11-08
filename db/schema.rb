12fc00f6162d2b663b7689bd3252c78aa5d8959a
Shirley DeCesari <shirley.decesari@gmail.com>
2022-11-07 17:31:22 -0800

    Corrected subscription migration and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_08_004544) do
+ActiveRecord::Schema.define(version: 2022_11_08_012254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,12 +29,12 @@ ActiveRecord::Schema.define(version: 2022_11_08_004544) do
     t.float "price"
     t.string "status"
     t.string "frequency"
-    t.bigint "customers_id"
-    t.bigint "teas_id"
+    t.bigint "customer_id"
+    t.bigint "tea_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["customers_id"], name: "index_subscriptions_on_customers_id"
-    t.index ["teas_id"], name: "index_subscriptions_on_teas_id"
+    t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
+    t.index ["tea_id"], name: "index_subscriptions_on_tea_id"
   end
 
   create_table "teas", force: :cascade do |t|
@@ -46,6 +46,6 @@ ActiveRecord::Schema.define(version: 2022_11_08_004544) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "subscriptions", "customers", column: "customers_id"
-  add_foreign_key "subscriptions", "teas", column: "teas_id"
+  add_foreign_key "subscriptions", "customers"
+  add_foreign_key "subscriptions", "teas"
 end
