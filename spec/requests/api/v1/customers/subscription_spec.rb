df9974060d8054b7d4c56c5382375a77ababcb18
Shirley DeCesari <shirley.decesari@gmail.com>
2022-11-08 14:30:33 -0800

    Add happy and sad path testing to subscription controller

--- spec/requests/api/v1/customers/subscription_spec.rb
+++ spec/requests/api/v1/customers/subscription_spec.rb
@@ -17,9 +17,44 @@ RSpec.describe 'Create Subscription' do
 
       post "/api/v1/customers/#{customer.id}/subscribe", headers: headers, params: JSON.generate(sub_params)
 
-      # binding.pry
       expect(response).to be_successful
       expect(response.status).to eq(201)
+
+      result = JSON.parse(response.body, symbolize_names: true)
+      # binding.pry
+      expect(result).to have_key(:data)
+      expect(result[:data]).to have_key(:id)
+      expect(result[:data]).to have_key(:type)
+      expect(result[:data]).to have_key(:attributes)
+      expect(result[:data][:attributes]).to have_key(:title)
+      expect(result[:data][:attributes][:title]).to be_a(String)
+      expect(result[:data][:attributes]).to have_key(:price)
+      expect(result[:data][:attributes][:price]).to be_a(Float)
+      expect(result[:data][:attributes]).to have_key(:status)
+      expect(result[:data][:attributes][:status]).to be_a(String)
+      expect(result[:data][:attributes]).to have_key(:frequency)
+      expect(result[:data][:attributes][:frequency]).to be_a(String)
     end
   end 
+
+  describe 'sad path' do 
+    it 'returns an error when params are missing' do 
+      customer = Customer.create!(first_name: 'shirley', last_name: 'decesari', email: 'shirley@me.com', address: 'a place')
+      tea = Tea.create!(title: 'green tea', description: 'it is green', temperature: 140, brew_time: '2 minutes')
+      sub_params = { 
+        title: 'green tea subscription', 
+        price: 18.00,
+        status: '',
+        frequency: 'weekly',
+        tea_id: tea.id,
+        customer_id: customer.id
+      }
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post "/api/v1/customers/#{customer.id}/subscribe", headers: headers, params: JSON.generate(sub_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+    end 
+  end
 end
