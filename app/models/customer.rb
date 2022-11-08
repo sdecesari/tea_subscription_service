52893753d41ee03f23414045ee00d387cf178472
Shirley DeCesari <shirley.decesari@gmail.com>
2022-11-07 17:11:28 -0800

    Add customer model

---
+++ app/models/customer.rb
@@ -0,0 +1,9 @@
+class Customer < ApplicationRecord
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :email
+  validates_presence_of :address
+
+  has_many :subscriptions
+  has_many :teas, through: :subscriptions
+end 
