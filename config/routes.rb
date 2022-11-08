9267418b5a871303bce41a9444279bb37bff1150
Shirley DeCesari <shirley.decesari@gmail.com>
2022-11-08 14:49:34 -0800

    Add update route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
       post '/customers/:customer_id/subscribe', to: 'subscriptions#create'
+      patch '/customers/:customer_id/cancel', to: 'subscriptions#update'
     end
   end
 end
