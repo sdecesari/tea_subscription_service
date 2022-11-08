f755e3b8008318a7a89338d4ed16d34d2eef9a22
Shirley DeCesari <shirley.decesari@gmail.com>
2022-11-07 17:06:38 -0800

    Add tea model

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -10,6 +10,6 @@ RSpec.describe Tea do
 
   describe 'relationships' do 
     it { should have_many(:subscriptions) }
-    it { should have_many(:teas).through(:subscriptions) }
+    it { should have_many(:customers).through(:subscriptions) }
   end 
 end
