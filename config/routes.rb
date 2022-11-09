33a7ea8a9b82946fa573099f62f80b6f58d08b9d
Shirley DeCesari <shirley.decesari@gmail.com>
2022-11-08 16:35:38 -0800

    Add subscription index route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do 
       post '/customers/:customer_id/subscribe', to: 'subscriptions#create'
       patch '/customers/:customer_id/cancel', to: 'subscriptions#update'
+      get '/customers/:customer_id/subscriptions', to: 'subscriptions#index'
     end
   end
 end
